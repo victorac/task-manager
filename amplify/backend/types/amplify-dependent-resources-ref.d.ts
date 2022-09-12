96c4fb9833babfb3acf3ea780cd3e9cfea9f9a3a
Victor <victorde.ac@gmail.com>
2022-09-12 16:19:07 -0300

    update project

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "taskmanager": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
